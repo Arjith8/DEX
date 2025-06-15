32030d1152ecc45eaf275ef0573612192e167fa7
Arjithar8 <arjithar8@gmail.com>
2025-06-15 16:43:23 +0530

    Updated script file to generate new keypairs while no longer overriding them

--- src/bin/create_mint_account.rs
+++ src/bin/create_mint_account.rs
@@ -2,21 +2,25 @@ use std::{fs, path::Path};
 
 use dotenvy::dotenv;
 use solana_sdk::{signature::Keypair};
+use nanoid::nanoid;
 
 fn main() {
     dotenv()
         .expect("Failed to load environment variables from .env file");
 
-    let mint_keypair = Keypair::new().to_base58_string();
+    let keypair = Keypair::new().to_base58_string();
 
     let keys_dir = Path::new("./keys");
     if fs::exists(keys_dir).is_ok_and(|exists| !exists) {
         fs::create_dir(keys_dir).expect("Failed to create keys directory");
-    }
+    };
 
-    let path = Path::new("./keys/mint_keypair");
+    let id = nanoid!(10);
+    let filename = format!("mint_keypair{}", id);
 
-    fs::write(path, &mint_keypair).expect("Failed to write mint keypair to file");
+    let path = keys_dir.join(filename);
+
+    fs::write(path, &keypair).expect("Failed to write mint keypair to file");
         
-    println!("Mint Account Public Key: {}", mint_keypair);
+    println!("Mint keypair has been successfully created");
 }
